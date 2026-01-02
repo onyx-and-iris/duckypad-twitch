4f087a035875b709e8259054bfa3fa4aaf9a6749
Onyx and Iris <75868496+onyx-and-iris@users.noreply.github.com>
2026-01-02 20:51:50 +0000

    upd docstrings

--- duckypad_twitch/util.py
+++ duckypad_twitch/util.py
@@ -1,5 +1,7 @@
 def ensure_obsws(func):
-    """ensure an obs websocket connection has been established"""
+    """ensure an obs websocket connection has been established
+
+    Used as a decorator for functions that require an obs websocket connection"""
 
     def wrapper(self, *args):
         if self.request is None:
@@ -11,8 +13,11 @@ def ensure_obsws(func):
 
     return wrapper
 
+
 def ensure_mixer_fadeout(func):
-    """ensure mixer fadeout is stopped before proceeding"""
+    """ensure mixer is faded out before proceeding (disable monitor speaker)
+
+    Used as a decorator for functions that require the mixer to be faded out"""
 
     def wrapper(self, *args):
         if self.mixer.lr.mix.fader > -90:
@@ -20,6 +25,7 @@ def ensure_mixer_fadeout(func):
         return func(self, *args)
 
     return wrapper
+
 
 def to_snakecase(scene_name: str) -> str:
     """Convert caplitalized words to lowercase snake_case"""
